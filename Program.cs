5804544a864ba237fad7232a36e5b247fb05509c
ShiftLock <ukraina65485@gmail.com>
2026-09-18 21:36:20 +0500

    First method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-Console.WriteLine("РТФ - ЛУЧШЕ ВСЕХ");
+using System;
+
+class Program
+{
+    static int SumTwoNumbers()
+    {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        return a + b;
+    }
+
+    static void Main()
+    {
+        int result = SumTwoNumbers();
+        Console.WriteLine($"Сумма: {result}");
+    }
+}
